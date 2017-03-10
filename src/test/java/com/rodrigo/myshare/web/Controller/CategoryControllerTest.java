fb9759dd401991430ca937d44e04122e2ad4c598
Rodrigo <romacas@hotmail.co.uk>
2017-03-10 19:44:48 +0000

    commit

---
+++ src/test/java/com/rodrigo/myshare/web/Controller/CategoryControllerTest.java
@@ -0,0 +1,10 @@
+package com.rodrigo.myshare.web.Controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Rodrigo on 10/03/2017.
+ */
+public class CategoryControllerTest {
+
+}
---
+++ src/test/java/com/rodrigo/myshare/web/Controller/RecipeControllerTest.java
@@ -0,0 +1,8 @@
+package com.rodrigo.myshare.web.Controller;
+
+import static org.junit.Assert.*;
+
+
+public class RecipeControllerTest {
+
+}
