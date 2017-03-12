0c8022a74ff8201497e39a5fbc36602de16823b6
Rodrigo <romacas@hotmail.co.uk>
2017-03-12 22:00:30 +0000

    Add recipe controller test

--- src/test/java/com/rodrigo/myshare/web/Controller/RecipeControllerTest.java
+++ src/test/java/com/rodrigo/myshare/web/Controller/RecipeControllerTest.java
@@ -1,8 +1,31 @@
 package com.rodrigo.myshare.web.Controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import static org.junit.Assert.*;
 
 
 public class RecipeControllerTest {
+
+    private MockMvc mockMvc;
+    private RecipeController controller;
+
+    @Before
+    public void setup() {
+        controller = new RecipeController();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void homeShouldRenderIndexPage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(view().name("recipe/index"));
+    }
 
 }
