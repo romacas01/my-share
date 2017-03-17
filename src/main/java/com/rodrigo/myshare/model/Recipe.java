c98a3b6f99c3748eaf35fcd49b6d633e451966d4
Rodrigo <romacas@hotmail.co.uk>
2017-03-17 19:22:21 +0000

    Fix Recipe entity

--- src/main/java/com/rodrigo/myshare/model/Recipe.java
+++ src/main/java/com/rodrigo/myshare/model/Recipe.java
@@ -2,6 +2,7 @@ package com.rodrigo.myshare.model;
 
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,4 +27,90 @@ public class Recipe {
     private LocalDateTime dateUploaded = LocalDateTime.now();
     private String username;
     private String hash;
+
+    public Recipe(){}
+
+    public String getTimeSinceUploaded() {
+        String unit = "";
+        LocalDateTime now = LocalDateTime.now();
+        long diff;
+        if((diff = ChronoUnit.SECONDS.between(dateUploaded,now)) < 60){
+            unit = "secs";
+        } else if ((diff = ChronoUnit.MINUTES.between(dateUploaded,now)) < 60) {
+            unit = "mins";
+        } else if ((diff = ChronoUnit.HOURS.between(dateUploaded,now)) < 24) {
+            unit = "hours";
+        } else if ((diff = ChronoUnit.DAYS.between(dateUploaded,now)) < 30) {
+            unit = "days";
+        } else if ((diff = ChronoUnit.MONTHS.between(dateUploaded,now)) < 12) {
+            unit = "months";
+        } else{
+            diff = ChronoUnit.YEARS.between(dateUploaded,now);
+        }
+        return String.format("%d %s",diff,unit);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDateTime dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
 }
