6a78afc524ea478db57a872f4f8d0dfa2c5c487e
Rodrigo <romacas@hotmail.co.uk>
2017-03-19 22:20:11 +0000

    Add session factory

--- src/main/java/com/rodrigo/myshare/web/Controller/CategoryController.java
+++ src/main/java/com/rodrigo/myshare/web/Controller/CategoryController.java
@@ -1,6 +1,9 @@
 package com.rodrigo.myshare.web.Controller;
 
 import com.rodrigo.myshare.model.Category;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,14 @@ import java.util.List;
 @Controller
 public class CategoryController {
 
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @SuppressWarnings("unchecked")
     @RequestMapping("/categories")
     public String listCategories(Model model) {
-        List<Category> categories = new ArrayList<>();
+        Session session = sessionFactory.openSession();
+        List<Category> categories = session.createCriteria(Category.class).list();
         model.addAttribute("categories", categories);
         return "category/categories";
     }
